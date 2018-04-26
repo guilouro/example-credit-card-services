526284de438025261191459280e76e2752b8d87a
Guilherme Louro <guilherme-louro@hotmail.com>
2018-04-26 14:52:09 -0300

    Created CardSimulator

---
+++ client/src/components/CardSimulator.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TweenLite, TimelineLite, CSSPlugin } from 'gsap';
+
+class CardSimulator extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.onFlip = this.onFlip.bind(this);
+    }
+
+    componentDidMount() {
+        CSSPlugin.defaultTransformPerspective = 1000;
+        TweenLite.set(this.back, { rotationY: -180 });
+        this.tl = new TimelineLite({ paused: true });
+        this.tl
+            .to(this.front, 0.4, { rotationY: 180 })
+            .to(this.back, 0.4, { rotationY: 0 }, 0);
+
+        this.onFlip(this.props.flip);
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (newProps.flip !== this.props.flip) {
+            this.onFlip(newProps.flip);
+        }
+    }
+
+    onFlip(flip) {
+        return flip ? this.tl.play() : this.tl.reverse();
+    }
+
+    render() {
+        return (
+            <div className="card" ref={(card) => { this.card = card; }}>
+                <div className="card__front" ref={(front) => { this.front = front; }}>
+                    <div className="card__number">
+                        {this.props.number.padEnd(19, '*')}
+                    </div>
+                    <div className="card__expiration">
+                        {this.props.expiration}
+                    </div>
+                    <div className="card__company">
+                        {this.props.company}
+                    </div>
+                </div>
+                <div className="card__back" ref={(back) => { this.back = back; }}>
+                    <div className="card__company">
+                        {this.props.cvv.padEnd(3, '*')}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+CardSimulator.defaultProps = {
+    number: '4454 4468 6658 6698',
+    expiration: '',
+    company: '',
+    cvv: '',
+    flip: false,
+};
+
+CardSimulator.propTypes = {
+    number: PropTypes.string,
+    expiration: PropTypes.string,
+    company: PropTypes.string,
+    cvv: PropTypes.string,
+    flip: PropTypes.bool,
+};
+
+export default CardSimulator;
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CardSimulator from 'components/CardSimulator';
 
 const Main = props => (
     <React.Fragment>
         <header>
-            Upnid - Credit Cards
+            <div className="header__name">
+                Upnid - Credit Cards
+            </div>
+            <div className="header__links">
+                1
+            </div>
         </header>
 
-        <section className="container">
+        <section className="content">
+
+            <div className="form">
+                <CardSimulator />
+            </div>
+
+            <form className="card__form">
+                <div className="form__row">
+                    <input type="text" name="number" />
+                    <input type="text" name="company" />
+                </div>
+                <div className="form__row">
+                    <input type="text" name="cvv" />
+                    <input type="text" name="expiration" />
+                    <button>SUBMIT</button>
+                </div>
+            </form>
+
             {props.children}
         </section>
     </React.Fragment>
 );
-
 
 Main.propTypes = {
     children: PropTypes.object,
