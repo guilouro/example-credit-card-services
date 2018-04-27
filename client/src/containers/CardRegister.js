f7bad85dbfe3273b9ab10749002556132ac0422c
Guilherme Louro <guilherme-louro@hotmail.com>
2018-04-26 21:44:21 -0300

    CardRegister as stateless

--- client/src/containers/CardRegister.js
+++ client/src/containers/CardRegister.js
@@ -4,67 +4,55 @@ import { connect } from 'react-redux';
 import CardSimulator from 'components/CardSimulator';
 import { toFlip, change } from 'actions/card-register';
 
-class CardRegister extends React.PureComponent {
-    constructor(props) {
-        super(props);
-    }
+const CardRegister = props => (
+    <div className="card__register">
+        <CardSimulator
+            {...props.card}
+        />
 
-    onChange(e, name) {
-        this.props.change(e.target.value, name);
-    }
-
-    render() {
-        return (
-            <div className="card__register">
-                <CardSimulator
-                    {...this.props.card}
-                />
-
-                <form className="card__form">
-                    <div className="form__row">
-                        <div className="form__field">
-                            <input
-                                type="text"
-                                placeholder="NUMBER"
-                                value={this.props.card.number}
-                                onChange={e => this.onChange(e, 'number')}
-                            />
-                        </div>
-                        <div className="form__field">
-                            <input
-                                type="text"
-                                placeholder="FULL NAME"
-                                value={this.props.card.company}
-                                onChange={e => this.onChange(e, 'company')}
-                            />
-                        </div>
-                    </div>
-                    <div className="form__row">
-                        <div className="form__field">
-                            <input
-                                type="text"
-                                placeholder="MM/YY"
-                                value={this.props.card.expiration}
-                                onChange={e => this.onChange(e, 'expiration')}
-                            />
-                        </div>
-                        <div className="form__field">
-                            <input
-                                type="text"
-                                placeholder="CVV"
-                                value={this.props.card.cvv}
-                                onChange={e => this.onChange(e, 'cvv')}
-                                onFocus={() => this.props.toFlip(true)}
-                                onBlur={() => this.props.toFlip(false)}
-                            />
-                        </div>
-                        <div className="form__button">SUBMIT</div>
-                    </div>
-                </form>
+        <form className="card__form">
+            <div className="form__row">
+                <div className="form__field">
+                    <input
+                        type="text"
+                        placeholder="NUMBER"
+                        value={props.card.number}
+                        onChange={e => props.change(e.target.value, 'number')}
+                    />
+                </div>
+                <div className="form__field">
+                    <input
+                        type="text"
+                        placeholder="FULL NAME"
+                        value={props.card.company}
+                        onChange={e => props.change(e.target.value, 'company')}
+                    />
+                </div>
             </div>
-        );
-    }
-}
+            <div className="form__row">
+                <div className="form__field">
+                    <input
+                        type="text"
+                        placeholder="MM/YY"
+                        value={props.card.expiration}
+                        onChange={e => props.change(e.target.value, 'expiration')}
+                    />
+                </div>
+                <div className="form__field">
+                    <input
+                        type="text"
+                        placeholder="CVV"
+                        value={props.card.cvv}
+                        onChange={e => props.change(e.target.value, 'cvv')}
+                        onFocus={() => props.toFlip(true)}
+                        onBlur={() => props.toFlip(false)}
+                    />
+                </div>
+                <div className="form__button">SUBMIT</div>
+            </div>
+        </form>
+    </div>
+);
 
 const mapStateToProps = ({ cardRegister }) => ({ card: cardRegister });
 
